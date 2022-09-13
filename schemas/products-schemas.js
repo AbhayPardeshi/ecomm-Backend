d7f4edcb5fe942e44dbe73aa14b1aa32ac9ebb9a
AbhayPardeshi <abhaypardeshi2210@gmail.com>
2022-09-13 12:02:38 +0530

    schema updated
    added productcolor and islatest

--- schemas/products-schemas.js
+++ schemas/products-schemas.js
@@ -11,10 +11,12 @@ const productSchema = new Schema({
   discountedPrice: String,
   rating: String,
   totalRatings: String,
+  productColor: String,
   isAvailable: { type: Boolean, default: true },
   isWishlisted: { type: Boolean, default: false },
   isAddedToCart: { type: Boolean, default: false },
   isFastDelivery: { type: Boolean, default: false },
+  isLatest: { type: Boolean, default: false },
   availableSize: {
     type: [String],
     default: ["XS", "S", "L", "XL", "XXL", "3XL"],
