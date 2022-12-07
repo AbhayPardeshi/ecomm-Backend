542e648474b134ef1158e99556e425c23699fb6a
AbhayPardeshi <abhaypardeshi2210@gmail.com>
2022-12-07 10:22:41 +0530

    changes

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ let app = express();
 const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
-app.use(function (req, res, next) {
+app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 app.set("json spaces", 4);
