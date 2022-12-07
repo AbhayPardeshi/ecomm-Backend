81e35e56f8dec3f766b2a85f021f8a91a6280c7a
AbhayPardeshi <abhaypardeshi2210@gmail.com>
2022-12-07 09:26:19 +0530

    cors changes

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.set("json spaces", 4);
 app.use(express.urlencoded({ extended: true })); // support encoded bodies
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 const { connectDB } = require("./db/db.connect.js");
 
